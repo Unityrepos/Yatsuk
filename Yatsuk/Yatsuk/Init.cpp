b95b6a1a0b60f93f089dfd4c788d4e9442f76049
Unityrepos <yatsukvitaliy@gmail.com>
2020-11-11 21:41:01 +0200

    Init (don't work).

--- Yatsuk/Yatsuk/Init.cpp
+++ Yatsuk/Yatsuk/Init.cpp
@@ -1,1 +1,14 @@
 #include "Init.h"
+#include <vector>
+vector<void(*)()> Init::updateFunction(0);
+void Init::Update(void(*func)())
+{
+	Init::updateFunction.push_back(func);
+}
+void Init::Update()
+{
+	for (auto i = std::begin(Init::updateFunction);i!=std::end(Init::updateFunction);++i)
+	{
+		(*i)();
+	}
+}
